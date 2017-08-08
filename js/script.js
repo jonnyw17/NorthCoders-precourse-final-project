7609fdfc2c4671bc9edd7e6c142a933210c9bcd0
jonnyw17 <jonny@spaceportx.com>
2017-08-08 10:14:04 +0100

    login area seperated to new page, htaccess added to redirect to user login area

--- js/script.js
+++ js/script.js
@@ -3,25 +3,33 @@
 function othername() {
     var first = document.getElementById("firstName").value;
     var last = document.getElementById("lastName").value;
-    var userName = first + ' ' + last;
+    var userName = `${first} ${last}`;
     var profileArea;
 
     if(first.length > 2 && last.length > 2){
-     document.getElementById('welcomeUser').innerHTML = "Hello " + userName;
+
+     document.getElementById('welcomeUser').innerHTML = "<h3>Hello " + userName ;
      $('.test').removeClass('failed');
      $('.test').removeClass('failed');
+     $('#alert').css({'display' : 'none'});
+     $('#welcomeUser').css({'display' : 'inline', "text-align" : 'center'})
      profileArea = true;
+
    }else {
-     document.getElementById('welcomeUser').innerHTML = "Please enter First and Last name";
+
+    //  document.getElementById('welcomeUser').innerHTML = "Please enter First and Last name";
+     $('#alert').fadeTo(500,1);
+     $('#welcomeUser').css({'display' : 'none'});
      $('.test').addClass('failed');
      $('.test').addClass('failed');
      profileArea = false;
    }
-    // $('#buttonUserDetails').on('click', function(){
-    //   $('.user-details').css({'display' : 'none'});
-    // })
+    if(profileArea === true){
+      $('.user-details').fadeOut(900, function (){
+        $(location).attr('href', 'index.html')
+    });
+  }
 }
-
 //html button
 $('#button-html').mouseenter(function(){
   $('.language-choice-css').fadeTo(300, 0.3)
@@ -31,6 +39,7 @@ $('#button-html').mouseleave(function(){
   $('.language-choice-css').fadeTo(00001, 1)
   $('.language-choice-javascript').fadeTo(00001, 1)
 });
+
 // css button
 $('#button-css').mouseenter(function(){
   $('.language-choice-html').fadeTo(300, 0.3)
